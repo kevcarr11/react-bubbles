aa366c687ece582298a98b706ca50c9844b7b19f
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-11-10 18:18:50 -0600

    crated login form to authenticate users

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Spinner } from 'reactstrap';
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleChange = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value 
+    })
+  }
+
+  const handleSubmit = (e) => {
+
+  }
+
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form>
+          <input type="text" name="username" placeholder=" User Name" value={userInfo.username} onchange={handleChange} />
+          <input type="password" name="password" placeholder="Password" value={userInfo.password} onchange={handleChange} />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 };
